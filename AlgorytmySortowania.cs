a56c0a403c5bab5ec2422d85d5c30d2f050c3d44
DesktopPC <mpalacz00@gmail.com>
2022-03-24 16:25:14 +0100

    Wykład5

---
+++ AlgorytmySortowania.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SelectSort
+{
+    class AlgorytmySortowania
+    {
+        // deklaracja metody SelectSort
+        public void SelectSort(ref float[] T)
+        {
+            int k;
+            float ElementPomocniczy;
+            // logiczny "podział" tablicy do sortowania
+            for (int i = 0; i < T.Length; i++)
+            {
+                /* 
+                 * tutaj jest prawdziwy niezmiennik obliczeń
+                 * T[0] <= ... <= T[i-1]
+                 * ustalenie indeksu dla pierwszego elementu w tablicy nieuporządkowanej
+                 */
+                k = i;
+                for (int j = i + 1; j < T.Length; j++)
+                    // sprawdzenie, czy T[k] jest większy od elementu T[j]
+                    if (T[k] > T[j])
+                        k = j; // okazało się, że j-ty element jest mniejszy od k
+                               // tutaj "pod indeksem" k będzie najmniejszy element z tablicy nieuporządkowanej
+                               // wymieniamy k-ty element z elementem i-tym
+                ElementPomocniczy = T[i];
+                T[i] = T[k];
+                T[k] = ElementPomocniczy;
+            }
+            // tutaj będzie spełniony niezmiennik obliczeń: T[0] <= T[1] <= ... <= T[T.Lenght - 1]
+        }
+        public int SelectSort(ref float[] T, int n)
+        {
+            int k;
+            float ElementPomocniczy;
+            int LicznikOperacjiDominujących = 0;
+            // logiczny "podział" tablicy do sortowania
+            for (int i = 0; i < n; i++)
+            {
+                /* 
+                 * tutaj jest prawdziwy niezmiennik obliczeń
+                 * T[0] <= ... <= T[i-1]
+                 * ustalenie indeksu dla pierwszego elementu w tablicy nieuporządkowanej
+                 */
+                k = i;
+                for (int j = i + 1; j < n; j++)
+                {
+                    LicznikOperacjiDominujących++;
+                    // sprawdzenie, czy T[k] jest większy od elementu T[j]
+                    if (T[k] > T[j]) // operacja dominująca, która decyduje o czasie sortowania
+                        k = j; // okazało się, że j-ty element jest mniejszy od k
+                }
+                // tutaj "pod indeksem" k będzie najmniejszy element z tablicy nieuporządkowanej
+                // wymieniamy k-ty element z elementem i-tym
+                ElementPomocniczy = T[i];
+                T[i] = T[k];
+                T[k] = ElementPomocniczy;
+            }
+            // tutaj będzie spełniony niezmiennik obliczeń: T[0] <= T[1] <= ... <= T[T.Lenght - 1]
+            // zwrócenie licznika opercji dominujących
+            return LicznikOperacjiDominujących;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -8,8 +8,27 @@ namespace SelectSort
 {
     class Program
     {
+        const int MaxRozmiarN = 100;
+        const int PrzyrostRozmiaruTablic = 5;
+        const int PróbaBadawcza = 100;
+        const float DolnaGranicaPrzedziału = 50;
+        const float GórnaGranicaPrzedziału = 500;
         static void Main(string[] args)
         {
+            Random rnd = new Random();
+            // schemat zapisu głównej części analizatora algorytmów sortowania
+            for (int l = 1; l < MaxRozmiarN; l += PrzyrostRozmiaruTablic)
+            {
+                // powtarzanie badanie dla l-tego rozmiaru tablicy
+                for(int k = 0; k < PróbaBadawcza;)
+                {
+                    // wypełnienie tablicy do sortowania wartościami losowymi
+                    for (int i = 0; i <= l; i++)
+                        T[i] = rnd.NextDouble() * (GórnaGranicaPrzedziału - DolnaGranicaPrzedziału) + DolnaGranicaPrzedziału;
+                    // wybieramy algorytm i go wywołujemy, a uzyskane wyniki przechowujemy dla dalszych potrzeb
+                    AlgorytmySortowania.selectSort(ref Task, l);
+                }
+            }
         }
     }
 }
